4a026d9f44fc27abadec70246ddd770ec0dad415
KimSungJun <jt2302@naver.com>
2026-08-16 16:06:41 +0900

    feat: 고유명사 마스킹 및 복원 로직 구현 #15

--- src/main/java/com/borderless/proxy/glossary/repository/GlossaryTermRepository.java
+++ src/main/java/com/borderless/proxy/glossary/repository/GlossaryTermRepository.java
@@ -2,9 +2,14 @@ package com.borderless.proxy.glossary.repository;
 
 import com.borderless.proxy.glossary.entity.GlossaryTerm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface GlossaryTermRepository extends JpaRepository<GlossaryTerm, Long> {
     List<GlossaryTerm> findByGlossaryId(Long glossaryId);
+
+    @Query("SELECT gt FROM GlossaryTerm gt JOIN gt.glossary g WHERE g.team.id = :teamId")
+    List<GlossaryTerm> findAllByTeamId(@Param("teamId") Long teamId);
 }
