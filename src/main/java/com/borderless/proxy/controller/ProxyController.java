793eb60e6a5683304f3b49eafbea6ed01047ed24
Lyudongwon <a89379903@gmail.com>
2026-08-19 22:09:57 +0900

    fix: ProxyController와 ProxyOrchestrator 인자 불일치로 컴파일 실패하는 문제 수정 #45
    process(request)로 1개 인자만 넘기고 있었으나 ProxyOrchestrator.process는
    (ProxyRequestDto, Long memberId) 2개를 받아 compileJava가 실패했다.
    
    memberId는 termMasker.maskText(memberId, ...)에 쓰이므로 제거할 수 없다.
    용어집이 회원별로 동작한다. 따라서 컨트롤러 쪽 @AuthenticationPrincipal을 복원한다.
    
    - SecurityConfig가 anyRequest().authenticated()라 이 엔드포인트는 인증이 필수다
    - CustomOAuth2UserService가 memberId 속성을 주입하므로 getAttribute는 정상 동작한다

--- src/main/java/com/borderless/proxy/controller/ProxyController.java
+++ src/main/java/com/borderless/proxy/controller/ProxyController.java
@@ -4,6 +4,8 @@ import com.borderless.proxy.dto.ProxyRequestDto;
 import com.borderless.proxy.dto.ProxyResponseDto;
 import com.borderless.proxy.service.ProxyOrchestrator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +21,11 @@ public class ProxyController {
 
     // 프론트엔드가 POST /api/proxy 로 요청을 보내면 이 함수가 받는다
     @PostMapping
-    public ProxyResponseDto proxy(@RequestBody ProxyRequestDto request) {
-        // 받은 요청을 두뇌에게 넘기고, 처리 결과를 그대로 돌려준다
-        return proxyOrchestrator.process(request);
+    public ProxyResponseDto proxy(@RequestBody ProxyRequestDto request,
+                                  @AuthenticationPrincipal OAuth2User principal) {
+        // 용어집 마스킹이 회원별로 동작하므로 memberId가 반드시 필요하다.
+        // CustomOAuth2UserService가 로그인 시점에 memberId 속성을 넣어준다.
+        Long memberId = principal.getAttribute("memberId");
+        return proxyOrchestrator.process(request, memberId);
     }
 }
